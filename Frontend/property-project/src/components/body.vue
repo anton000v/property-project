77083e936d2d1d480821211149b800e7d8425bf3
antonov <antonovdanil2000@gmail.com>
2020-04-01 15:42:19 +0300

    initial setting

---
+++ Frontend/property-project/src/components/body.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <TodoList v-bind:todo-list="todos" @delete-todo="deleteTodo"/>
+    <br>
+    <Testapi/>
+  </div>
+</template>
+
+<script>
+import TodoList from './TodoList.vue';
+import Testapi from './testapi.vue';
+
+export default{
+  props: ['Body'],
+  data(){
+    return {
+      todos: ['Ремонт', 'Something else', 'lalala']
+    }
+  },
+  components: {
+    TodoList,
+    Testapi
+  },
+  methods: {
+    deleteTodo(index){
+      this.todos.splice(index, 1);
+    }
+  }
+}
+</script>
---
+++ Frontend/property-project/src/components/testapi.vue
@@ -0,0 +1,31 @@
+<template>
+
+<p> {{ info }} </p>
+
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default{
+  props: ['Testapi'],
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    const axios = require('axios');
+
+      axios.get('http://127.0.0.1:8000/api/get-micro-districts').then(resp => {
+
+          console.log(resp.data);
+      });
+
+    // axios
+    //   .get('http://127.0.0.1:8000/api/get-micro-districts/')
+    //   .then(response => (this.info = response.data));
+  }
+}
+
+</script>
