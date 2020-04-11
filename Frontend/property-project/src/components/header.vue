95c40d88c4787bff084a073a36bb63d7b94a4516
Danil <antonovdanil2000@gmail.com>
2020-04-11 15:42:41 +0300

    some modifications

--- Frontend/property-project/src/components/header.vue
+++ Frontend/property-project/src/components/header.vue
@@ -5,7 +5,7 @@
 <nav class="navbar navbar-light bg-light">
   <a class="navbar-brand" href="#">
     <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-    Bootstrap
+    property-project header
   </a>
 </nav>
 
