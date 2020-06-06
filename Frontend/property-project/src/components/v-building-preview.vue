52d7577e44292082f4a45b139330a2e8876a429c
anton000v <antonovdanil2000@gmail.com>
2020-06-06 19:20:45 +0300

    Add simple preview image slider

--- Frontend/property-project/src/components/v-building-preview.vue
+++ Frontend/property-project/src/components/v-building-preview.vue
@@ -13,14 +13,18 @@
         <!-- </div> -->
         
         <div slot="top-right">
-        <button @click="$modal.hide('building-preview:'+building.slug)">
-            <!-- <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50"> -->
-                <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 18 18">
-                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-                </svg>
-                <span class="text-sm">(Esc)</span>
-            <!-- </div> -->
-        </button>
+            <button @click="$modal.hide('building-preview:'+building.slug)">
+                <!-- <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50"> -->
+                    <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 18 18">
+                    <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                    </svg>
+                    <span class="text-sm">(Esc)</span>
+                <!-- </div> -->
+            </button>
+        </div>
+
+        <div class='h-20'>
+            <vPreviewSlider/>
         </div>
 
         <div class="absolute inset-x-0 bottom-0 items-center text-center bg-gray-300 py-6">
@@ -32,7 +36,11 @@
 </template>
 
 <script>
+import vPreviewSlider from './v-preview-slider'
 export default {
+    components:{
+        vPreviewSlider
+    },
     props:{
         slug: {
             type: String,
--- Frontend/property-project/src/store/index.js
+++ Frontend/property-project/src/store/index.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import building from './modules/building'
 import buildingsSearchKit from './modules/buildingsSearchKit'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// import style
+import 'swiper/css/swiper.css'
+
 // import localStorage from './modules/localStorage'
 Vue.use(Vuex)
-
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 export default new Vuex.Store({
 
     modules:{
