71bfcf5bb26b7368b131f40672bd7edaa502574b
Danil <antonovdanil2000@gmail.com>
2021-03-28 17:55:52 +0300

    heating

--- apn-ui/src/components/v-buildings-search-kit.vue
+++ apn-ui/src/components/v-buildings-search-kit.vue
@@ -708,37 +708,26 @@
                 </div>
                 <div class="w-full md:w-1/4 px-3  border-l rounded">
                   <label
-                      class="block text-xs md:text-sm lg:text-base  font-bold text-gray-500  md:text-left mb-1 md:mb-0 pr-4"
+                      class="block text-xs md:text-sm lg:text-base text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
                       for="grid-last-name">
-                    Этажность
+                    Отопление
                   </label>
-                  <div class="w-4/5 md:w-full m-auto">
-                    <div class="flex  justify-center items-center">
-                      <div class="">
-                        <vInputSearch
-                            @valueChanged="search"
-                            @activateExtendedSearch="isExtendedSearchActivated=true"
-                            :addAction="addFindParam"
-                            :removeAction="removeFindParam"
-                            :sendParamName="numberOfStoreysBaseVariables.sendParamNameFrom"
-                            :removeKeyAction="removeFindParamKey"
-                            :isForExtendedSearch="true"
-                        />
-                      </div>
-                      <div class="px-2 text-xs md:text-sm lg:text-base">
-                        -
-                      </div>
-                      <div class="">
-                        <vInputSearch
-                            @valueChanged="search"
-                            @activateExtendedSearch="isExtendedSearchActivated=true"
-                            :addAction="addFindParam"
-                            :removeAction="removeFindParam"
-                            :sendParamName="numberOfStoreysBaseVariables.sendParamNameTo"
-                            :removeKeyAction="removeFindParamKey"
-                            :isForExtendedSearch="true"
-                        />
-                      </div>
+                  <div name="field" class="w-full">
+                    <div class="">
+                      <MultipleSelect
+                          @selectClose="search"
+                          @activateExtendedSearch="isExtendedSearchActivated=true"
+                          :dbValueKey="heatingBaseVariables.dbValueKey"
+                          :fieldChoiceText="heatingBaseVariables.choiceText"
+                          :dictKey="heatingBaseVariables.dictKey"
+                          :apiAddress="heatingBaseVariables.fullApiAddress"
+                          :sendParamName="heatingBaseVariables.sendParamName"
+                          :addAction="addFindParam"
+                          :removeAction="removeFindParam"
+                          :removeKeyAction="removeFindParamKey"
+                          :isForExtendedSearch="true"
+                          placeholder="Выберите отопление"
+                      />
                     </div>
                   </div>
                 </div>
