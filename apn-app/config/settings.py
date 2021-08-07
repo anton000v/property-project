ae6f81d82e8d9649ef2866f6e2e38bb4f5f3f0b6
Danil <antonovdanil2000@gmail.com>
2021-08-07 19:48:11 +0300

    Add api subdomain to allowed hosts

--- apn-app/config/settings.py
+++ apn-app/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'asnovostroi.com.ua']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'api.asnovostroi.com.ua']
 
 # Django corsheaders
 CORS_ORIGIN_ALLOW_ALL = True
